037c8419a568ed8790d92b73656c9d5212684504
Joan <joancer2@gmail.com>
2020-04-22 20:06:30 +0200

    scenas

---
+++ AVD/Assets/Joc_Nou/Scripts/Scenes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public void cambiarescena(string nombreescena)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
