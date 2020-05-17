8252ed25fa9bd056b3d26e26e19c9b739a2930c9
Joan <joancer2@gmail.com>
2020-05-17 14:52:09 +0200

    FIN

---
+++ AVD/Assets/Joc_Nou/chest/OpenChest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChest : MonoBehaviour
+{
+
+    public AudioClip openChestSound;
+    public GameObject TresureChest;
+    private Animator anim;
+    private bool hasPlayer;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(hasPlayer && Input.GetKeyDown("e")) 
+        {
+            anim.Play("openChest");
+        }
+    }
+    private void onTriggerEnter(Collider col)
+    {
+        Debug.Log(col.CompareTag("Player"));
+        if (col.CompareTag("Player"))
+        {
+            hasPlayer = true;
+        }
+    }
+
+    private void onTriggerExit(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            hasPlayer = false;
+        }
+    }
+}
---
+++ AVD/Assets/Joc_Nou/exit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    public void onClick()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+}
